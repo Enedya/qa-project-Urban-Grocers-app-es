3fd28a79c6d6c34e6320f050a766448696a76f1f
Enedy <enedy93@gmail.com>
2024-06-21 10:47:07 -0400

    Actualizacion proyecto 7.

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-dab00ad5-e6d0-42b3-9956-3e2f71ec14ba.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-d88e684d-3ce1-4d2b-a22c-8b1b3bb32c3c.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users" # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = "/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
